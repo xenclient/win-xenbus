e076d8fced72c1d3e1aa3bf2a2b93773c8d1b23c
Steve Meisner <steve.meisner@citrix.com>
2014-04-30 14:54:30 -0400

    Re-add patch to not deref null descriptor pointer when fetching grant ref

--- src/xenbus/gnttab.c
+++ src/xenbus/gnttab.c
@@ -440,19 +440,17 @@ GnttabReference(
     )
 {
     UNREFERENCED_PARAMETER(Context);
-<<<<<<< HEAD
+
     //
     // If the Descriptor is NULL, just return 0 for the ref
     //
     if (Descriptor == NULL)
         return 0;
-=======
 
     ASSERT3U(Descriptor->Magic, ==, GNTTAB_DESCRIPTOR_MAGIC);
     ASSERT3U(Descriptor->Reference, >=, GNTTAB_RESERVED_ENTRY_COUNT);
     ASSERT3U(Descriptor->Reference, <, Context->FrameCount * GNTTAB_ENTRY_PER_FRAME);
 
->>>>>>> upstream/master
     return (ULONG)Descriptor->Reference;
 }
 
